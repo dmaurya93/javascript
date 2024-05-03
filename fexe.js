0fa558967da44ef1dab0b34344f4493c6993c970
Dharam Maurya <dmaurya@localhost.localdomain>
2024-05-03 18:34:25 +0530

    first commit

---
+++ fexe.js
@@ -0,0 +1,19 @@
+let a = 50
+console.log(a)
+var b = 60
+console.log(a+b)
+x = "Dharam Maurya"
+console.log(x)
+
+let pattern =  "";
+for (i=1;i<=5;i++)
+{
+	for (j=1;j<=i;j++)
+	{
+		//console.log(j)
+		pattern += j+" ";
+	}
+	//console.log("\n");
+	pattern += "\n";
+}
+console.log(pattern);
